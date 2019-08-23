42259e2c67004e98c34c7f9187adde14ba7b7ccd
Mitchell Brown <mitchell.e.brown@usace.army.mil>
2019-08-23 16:35:54 -0500

    Implemented a fix for reading .win/.pre files for Oceanweather wind format input.

--- trunk/source/main/CMS2D_main.f90
+++ trunk/source/main/CMS2D_main.f90
@@ -41,7 +41,7 @@
     !NOTE: Change variables Below to update header information
     version  = 5.1            !CMS version
     revision = 8              !Revision number
-    rdate    = '07/29/2019'
+    rdate    = '08/23/2019'
 
 #ifdef DEV_MODE
     release  = .false.
