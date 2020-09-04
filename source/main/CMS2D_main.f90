bf4f882691cc2a39615a262b1d27b555e6509df1
mebrown998 <mebrown998@gmail.com>
2020-09-04 09:49:54 -0500

    Bug fix for isolated water cells. Switch to screen output in lieu of 'isolated.txt' external file.

--- source/main/CMS2D_main.f90
+++ source/main/CMS2D_main.f90
@@ -39,8 +39,8 @@
     !NOTE: Change variables Below to update header information
     !If bug fix for release version, make revision a float (ie 15.1, 15.2, etc).   MEB  07/30/20
     version  = 5.2           !CMS version
-    revision = 2.0           !Revision number
-    rdate    = '08/13/2020'
+    revision = 2.1           !Revision number
+    rdate    = '09/04/2020'
     
 #ifdef _WIN32
     machine='Windows'
@@ -689,7 +689,7 @@
 7015  format(' Coupled Hydrodynamic, Wave, and Sediment Transport Model  ')
 7016  format('               Last updated - ',A10)
 7017  format('       For the latest version of CMS please visit          ')
-7018  format('          http://cirp.usace.army.mil/products/             ')
+7018  format('         https://cirp.usace.army.mil/products/             ')
 
 9001  format('      By using this software the user has agreed to the    ')
 9002  format('      terms and conditions of CMS license agreement.       ') 
