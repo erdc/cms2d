c5ecec49f9df051bee181252af0ad15b059aefab
mebrown998 <mebrown998@gmail.com>
2020-09-23 14:58:59 -0500

    Created descriptive folders within the project to hold individual files.  Add missing file,'unitconv_def.f90' (evidently not presently used).

--- source/utilities/unitconv_def.f90
+++ source/utilities/unitconv_def.f90
@@ -9,12 +9,12 @@ module unitconv_def
 
     !--- Distances ----
     !per meter
-    real(ikind),parameter :: ftpermm = 0.00328084_ikind  !foot per milimeter
-    real(ikind),parameter :: ftperm = 3.28084_ikind      !foot per meter
-    real(ikind),parameter :: inperm = 39.3701_ikind      !Inch per meter
-    real(ikind),parameter :: ydperm = 1.09361_ikind      !Yard per meter
-    real(ikind),parameter :: miperm = 0.000621371_ikind  !Mile per meter
-    real(ikind),parameter :: nmiperm = 0.000539957_ikind !Nautical mile per meter
+    real(ikind),parameter :: ftpermm = 0.00328084_ikind   !foot per milimeter
+    real(ikind),parameter :: ftperm = 3.28084_ikind       !foot per meter
+    real(ikind),parameter :: inperm = 39.3701_ikind       !Inch per meter
+    real(ikind),parameter :: ydperm = 1.09361_ikind       !Yard per meter
+    real(ikind),parameter :: miperm = 0.000621371_ikind   !Mile per meter
+    real(ikind),parameter :: nmiperm = 0.000539957_ikind  !Nautical mile per meter
     !per foot
     real(ikind),parameter :: inperft = 12.0_ikind         !Inch per foot
     real(ikind),parameter :: ydperft = 0.333333_ikind     !Yard per foot
@@ -37,29 +37,29 @@ module unitconv_def
     !real(ikind),parameter :: rad2deg = 180.0_ikind/pikind
     
     !--- Mass ---
-    real(ikind),parameter :: lbperkg = 2.20462262_ikind     !lb per kg
-    real(ikind),parameter :: kgperlb = 0.45359237_ikind     !kg per lb
-    real(ikind),parameter :: kgpertonUK = 1016.046909_ikind !kg per tonUK
-    real(ikind),parameter :: kgpertonLong = kgpertonUK      !kg per tonlong
-    real(ikind),parameter :: lbpertonUK = 2240.0_ikind      !lb per tonUK
-    real(ikind),parameter :: lbpertonLong = lbpertonUK      !lb per tonLong
-    real(ikind),parameter :: kgpertonUS = 907.18474_ikind   !kg per tonUS
-    real(ikind),parameter :: kgpertonShort = kgpertonUS     !kg per tonShort
-    real(ikind),parameter :: lbpertonUS = 2000.0_ikind      !lb per tonUS
-    real(ikind),parameter :: lbpertonShort = lbpertonUS     !lb per tonShort
-    real(ikind),parameter :: kgpertonne = 1000.0_ikind      !kg per tonne
-    real(ikind),parameter :: lbpertonne = 2204.622622_ikind !lb per tonne
+    real(ikind),parameter :: lbperkg = 2.20462262_ikind      !lb per kg
+    real(ikind),parameter :: kgperlb = 0.45359237_ikind      !kg per lb
+    real(ikind),parameter :: kgpertonUK = 1016.046909_ikind  !kg per tonUK
+    real(ikind),parameter :: kgpertonLong = kgpertonUK       !kg per tonlong
+    real(ikind),parameter :: lbpertonUK = 2240.0_ikind       !lb per tonUK
+    real(ikind),parameter :: lbpertonLong = lbpertonUK       !lb per tonLong
+    real(ikind),parameter :: kgpertonUS = 907.18474_ikind    !kg per tonUS
+    real(ikind),parameter :: kgpertonShort = kgpertonUS      !kg per tonShort
+    real(ikind),parameter :: lbpertonUS = 2000.0_ikind       !lb per tonUS
+    real(ikind),parameter :: lbpertonShort = lbpertonUS      !lb per tonShort
+    real(ikind),parameter :: kgpertonne = 1000.0_ikind       !kg per tonne
+    real(ikind),parameter :: lbpertonne = 2204.622622_ikind  !lb per tonne
     
     !--- Area ---
-    real(ikind),parameter :: ft2perm2 = 10.76391042_ikind !ft^2 per m^2
-    real(ikind),parameter :: m2perft2 = 0.09290304_ikind  !m^2 per ft^2
+    real(ikind),parameter :: ft2perm2 = 10.76391042_ikind    !ft^2 per m^2
+    real(ikind),parameter :: m2perft2 = 0.09290304_ikind     !m^2 per ft^2
     
     !--- Volume ---
-    real(ikind),parameter :: ft3perm3 = 35.31472_ikind    !ft^3 per m^3
-    real(ikind),parameter :: m3perft3 = 0.02831680_ikind  !m^3 per ft^3
+    real(ikind),parameter :: ft3perm3 = 35.31472_ikind       !ft^3 per m^3
+    real(ikind),parameter :: m3perft3 = 0.02831680_ikind     !m^3 per ft^3
     
     !--- Density ----
     real(ikind),parameter :: kgft3perlbm3 = 16.02_ikind      !kg/m^3 to lb/ft^3
     real(ikind),parameter :: lbm3perkgft3 = 0.06242796_ikind !lb/ft^3 to kg/m^3
     
-endmodule unitconv_def
+end module unitconv_def
