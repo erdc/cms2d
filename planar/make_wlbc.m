d0aad90a7a471a70987d377a5ad8cebc27c872a6
dangerbaby <bradleydjohnson@gmail.com>
2020-12-14 12:29:53 -0500

    cleaned up and ignored planar

--- planar/make_wlbc.m
+++
@@ -1,3 +0,0 @@
-function g = make_wlbc(g)
-
-
--- planar/plot_vel.m
+++
@@ -1,9 +0,0 @@
-if g.plot.vel
-  fs = 24;
-  hh=figure;
-  quiver(g.grid.x_c,g.grid.y_c,out.vel.data2d.x(:,:,end),out.vel.data2d.y(:,:,end))
-  xlabel('$x [m]$','interpreter','latex','fontsize',fs)
-  ylabel('$y [m]$','interpreter','latex','fontsize',fs)
-  axis equal
-  
-end
