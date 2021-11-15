a0d816f3b17469f19d2ac3c315dc40b5debd008f
mebrown998 <mebrown998@gmail.com>
2021-11-15 12:18:41 -0600

    Bug fix
    Fixed error reading in WV_NESTING_CELLS and WV_OBSERVATION_CELLS card.  Increment number to match the version given to Aquaveo and their records.

--- source/main/CMS2D_main.f90
+++ source/main/CMS2D_main.f90
@@ -41,7 +41,7 @@
     version  = 5.2           ! CMS version         !For interim version
     revision = 12            ! Revision number
     bugfix   = 0             ! Bugfix number
-    rdate    = '11/08/2021'
+    rdate    = '11/15/2021'
 
     !Manipulate to get major and minor versions - MEB  09/15/20
     call split_real_to_integers (version, 2, major_version, minor_version)  !Convert version to two integer portions before and after the decimal considering 2 digits of precision.
